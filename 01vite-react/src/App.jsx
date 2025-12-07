bdaca5b764a44605bd935ac5e301bd2cbcc49ac3
varshneylove <lavvarshney2@gmail.com>
2025-12-07 13:14:04 +0530

    use of vite

---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,19 @@
+import Chai from "./chai"
+function App() {
+ 
+
+  return (
+  
+    <>
+    <Chai/>
+   <h1>Chai aur code with vite | LV </h1>
+   <p>Hello bhai smart banda hai tu </p>
+    
+    </>
+
+  
+
+  )
+}
+
+export default App
