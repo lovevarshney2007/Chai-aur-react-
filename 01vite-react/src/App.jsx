9b6635d019c3bb0eedd6bc0ed550cbba95708dae
varshneylove <lavvarshney2@gmail.com>
2025-12-07 19:40:43 +0530

    Build customReact and type

--- 01vite-react/src/App.jsx
+++ 01vite-react/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./chai"
 function App() {
- 
+  const username = "chai aur code"
 
   return (
   
     <>
     <Chai/>
-   <h1>Chai aur code with vite | LV </h1>
+   <h1>Chai aur code with vite | LV {username}</h1>
    <p>Hello bhai smart banda hai tu </p>
     
     </>
--- 01vite-react/src/main.jsx
+++ 01vite-react/src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App Lv</h1>
+        </div>
+    )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'http://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+
+
+const anotherElement = (
+    <a href="https://google.com" target='_black'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com',target: '_blank' },
+    'Click me to visit Google',
+    anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <App />
+    // <App/>
+    reactElement
+    // anotherElement
+    // ReactElement
+    // <MyApp/>
+    // MyApp()
  
 )
