7a683e3c9a12abc6beda0c7aa4d5a482afd9b48f
varshneylove <lovevarshney7002@gmail.com>
2025-12-27 21:45:00 +0530

    building pages

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -4,6 +4,12 @@ import Container from "./container/Container";
 import Logo from "./console.logo"
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login'
+import Post from './Post'
+import PostForm from './post-form/PostForm'
+import PostCard from '../PostCard'
+import AuthLayout from "./AuthLayout";
 
 export {
     Header,
@@ -11,5 +17,7 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    PostForm
 }
